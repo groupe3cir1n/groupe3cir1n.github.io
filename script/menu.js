967655f76a3d1245ef99f9f387f09af94e0357d8
AmalizzyDHY <amalizzy.dhy@gmail.com>
2023-05-17 14:54:46 +0200

    footer

--- script/menu.js
+++ script/menu.js
@@ -6,4 +6,13 @@ function affiche_heure(){
     let horloge = document.getElementById("heure");
 }
 
+function footer(numero){
+
+    let num = prompt("Si vous voulez appeler ce numéro : " + numero + " entrez le de nouveau dans le champs ci-dessous puis validez.", "+33");
+    if (num == numero) {
+      console.log("Vous appelez ce numéro : " + num + " !");
+    }
+
+}
+
 main();
