f2eb6276d4b2c0c67915ba4b8cb81dd414d2320e
AmalizzyDHY <amalizzy.dhy@gmail.com>
2023-05-24 14:28:18 +0200

    music footer

--- script/footer.js
+++ script/footer.js
@@ -3,20 +3,17 @@ function footer(numero){ //function qui se lance dans copy un numéro
     let num = prompt("Si vous voulez appeler ce numéro : " + numero + " entrez le de nouveau dans le champs ci-dessous puis validez.", "+33..."); //fenêtre prompt qui demande le numéro
     if (num == numero) { //Si l'utilisateur à bien entrer le numéro
       console.log("Vous appelez ce numéro : " + num + " !"); //Message dans la console
-      //music();
+      music();
     }
 
 }
 
 function music(){
 
-  var sound = new Howl({
-    src: ['https://youtu.be/JPZ-QyVBOf8'],
-    volume: 1.0,
-    onend: function () {
-      alert('We finished with the setup!');
-    }
-  });
-  sound.play()
+  const myAudio = document.createElement('audio');
+  myAudio.setAttribute('../Docslmgs/SONGS.mp3','audiofile.mp3');
+
+  myAudio.currentTime = 5;
+  myAudio.play();
 
 }
