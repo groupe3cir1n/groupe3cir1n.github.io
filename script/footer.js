e60923720f0ae0045e9cad35ac63813a5a5fd4cc
AmalizzyDHY <amalizzy.dhy@gmail.com>
2023-05-24 14:02:05 +0200

    music footer

--- script/footer.js
+++ script/footer.js
@@ -3,7 +3,7 @@ function footer(numero){ //function qui se lance dans copy un numéro
     let num = prompt("Si vous voulez appeler ce numéro : " + numero + " entrez le de nouveau dans le champs ci-dessous puis validez.", "+33..."); //fenêtre prompt qui demande le numéro
     if (num == numero) { //Si l'utilisateur à bien entrer le numéro
       console.log("Vous appelez ce numéro : " + num + " !"); //Message dans la console
-      music();
+      //music();
     }
 
 }
