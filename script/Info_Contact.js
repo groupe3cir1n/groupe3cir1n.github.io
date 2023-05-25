ae0c65b37c4bc5add03eee06563bb4b3a59561e6
AmalizzyDHY <amalizzy.dhy@gmail.com>
2023-05-25 12:22:49 +0200

    Info_Contact

---
+++ script/Info_Contact.js
@@ -0,0 +1,34 @@
+function prenom(){
+
+    let res = false ;
+    let value = document.getElementById("input_prenom") ;
+    //si il y a un espace et 2 majuscules
+}
+
+function mail(){
+
+    let res = false ;
+    let value = document.get
+
+
+}
+
+function txtarea (){
+
+    let res = false ;
+
+
+}
+
+function submit (){
+
+    if (prenom() == true){
+        if (mail() == true){
+            if (txtarea () == true){
+                //On peut appuyer sur envoyer
+                //lance le jeu ausshi
+            }
+        }
+    }
+
+}
