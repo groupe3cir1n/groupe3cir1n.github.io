3621e5fe8edca0040ec14c685dcb87c2083b565f
AmalizzyDHY <amalizzy.dhy@gmail.com>
2023-05-27 14:13:21 +0200

    projet

--- script/projet.js
+++ script/projet.js
@@ -1,3 +1,47 @@
+function changeImage1(){
+
+    if(document.getElementById('img1').value=="../DocsImgs/illu/globe-solid.png"){
+        img.src=="../DocsImgs/illu/Iot.png";
+    }
+    else if(docuemtn.getElementById('img1').value=="../DocsImgs/illu/Iot.png"){
+        img.src=="../DocsImgs/illu/globe-solid.png";
+    }
+
+}	
+
+function changeImage2(){
+
+    if(document.getElementById('img2').value=="../DocsImgs/illu/visemar.png"){
+        img.src=="mouton22.png";
+    }
+    else if(docuemtn.getElementById('img2').value=="mouton22.png"){
+        img.src=="../DocsImgs/illu/visemar.png";
+    }
+
+}	
+
+function changeImage3(){
+
+    if(document.getElementById('img3').value=="../DocsImgs/illu/seas.png"){
+        img.src=="mouton22.png";
+    }
+    else if(docuemtn.getElementById('img3').value=="mouton22.png"){
+        img.src=="../DocsImgs/illu/seas.png";
+    }
+
+}	
+
+function changeImage4(){
+
+    if(document.getElementById('img4').value=="../DocsImgs/illu/hpec.png"){
+        img.src=="mouton22.png";
+    }
+    else if(docuemtn.getElementById('img4').value=="mouton22.png"){
+        img.src=="../DocsImgs/illu/hpec.png";
+    }
+
+}
+
 function main(){
 
     const button = document.querySelector('.btn');
