8972af9dfaf6fc4229f331f6d7293a27a902e294
Balblaa <vdronne86@orange.fr>
2023-05-28 22:55:47 +0200

    membre / gratter ayoub

--- script/membres.js
+++ script/membres.js
@@ -1,11 +1,3 @@
-function get2DContext(id){
-
-        let canvas = document.getElementById(id);
-        let context = canvas.getContext("2d");
-        return context;
-
-}
-
 function main(){
 
     // création d'un cercle gris sur le potrait déjà présent
@@ -19,28 +11,21 @@ function main(){
     context.closePath();
 
     let cache = document.getElementById("couverture");
-    cache.addEventListener("hover", gratte);
+    cache.addEventListener("mousemove", gratte);
 
 }
 
-function gratte(){
+function get2DContext(id){
+
+    let canvas = document.getElementById(id);
+    let context = canvas.getContext("2d");
+    return context;
+
+}
+
+function gratte(e){
     let context = get2DContext("couverture");
-
-}
-
-function mouseCoords(ev){
-    if(ev.pageX || ev.pageY){
-            return {x:ev.pageX, y:ev.pageY};
-    }
-    return {
-            x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-            y:ev.clientY + document.body.scrollTop  - document.body.clientTop
-    };
-}
-
-function coordonnees(e,el) {
-    var coords = mouseCoords(e);
-    document.body.innerHTML += "<br />X: " + (coords.x - el.offsetLeft) + ", Y: " + (coords.y - el.offsetTop);
+    context.clearRect(e.clientX-284, e.pageY-368, 10, 10);
 }
 
 main();
