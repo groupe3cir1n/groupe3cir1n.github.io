baa1337aae38e05cd376888727f4a39d538ceff1
AmalizzyDHY <amalizzy.dhy@gmail.com>
2023-05-28 02:11:17 +0200

    projet

--- script/projet.js
+++ script/projet.js
@@ -2,15 +2,16 @@ function changeImage1(){
 
     let img = document.getElementById('img1').src ;
     console.log("canard");
+    console.log(img);
 
-    if( img == "../DocsImgs/illu/globe-solid.png" ){
-        img = "../DocsImgs/illu/Iot.png";
+    if( img == "http://127.0.0.1:5500/groupe3cir1n.github.io/DocsImgs/illu/globe-solid.png" ){
+        img = "octopus.png";
         console.log("poulpe");
     }
-    else if( img == "../DocsImgs/illu/Iot.png" ){
-        img = "../DocsImgs/illu/globe-solid.png";
+    else { if( img == "http://127.0.0.1:5500/groupe3cir1n.github.io/DocsImgs/illu/Iot.png" ){
+        img = "http://127.0.0.1:5500/groupe3cir1n.github.io/DocsImgs/illu/globe-solid.png";
         console.log("octopus");
-    }
+    }}
 
 }	
 
@@ -18,15 +19,16 @@ function changeImage2(){
 
     let img = document.getElementById('img2').src ;
     console.log("canard");
+    console.log(img);
 
     if( img == "../DocsImgs/illu/visemar.png" ){
         img = "../DocsImgs/illu/requin_baleine.jfif";
         console.log("poulpe");
     }
-    else if( img == "../DocsImgs/illu/requin_balein.jfif" ){
+    else {if( img == "../DocsImgs/illu/requin_balein.jfif" ){
         img = "../DocsImgs/illu/visemar.png";
         console.log("octopus");
-    }
+    }}
 
 }	
 
@@ -34,15 +36,16 @@ function changeImage3(){
 
     let img = document.getElementById('img3').src ;
     console.log("canard");
+    console.log(img);
 
     if( img == "../DocsImgs/illu/seas.png" ){
         img = "../DocsImgs/illu/phoque.png";
         console.log("poulpe");
     }
-    else if( img == "../DocsImgs/illu/phoque.png" ){
+    else {if( img == "../DocsImgs/illu/phoque.png" ){
         img = "../DocsImgs/illu/seas.png";
         console.log("octopus");
-    }
+    }}
 
 }	
 
@@ -50,15 +53,16 @@ function changeImage4(){
 
     let img = document.getElementById('img4').src ;
     console.log("canard");
+    console.log(img);
 
     if( img == "../DocsImgs/illu/hpec.png" ){
         img = "../DocsImgs/illu/octopus.png";
         console.log("poulpe");
     }
-    else if( img == "../DocsImgs/illu/octopus.png" ){
+    else {if( img == "../DocsImgs/illu/octopus.png" ){
         img = "../DocsImgs/illu/hpec.png";
         console.log("octopus");
-    }
+    }}
 
 }
 
