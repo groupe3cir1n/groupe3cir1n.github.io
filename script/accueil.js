7c2ce1bb809bab8d9b91fed75500779762415a14
Balblaa <vdronne86@orange.fr>
2023-05-28 15:20:41 +0200

    image agrandissement

--- script/accueil.js
+++ script/accueil.js
@@ -1,6 +1,18 @@
 function main(){
     let image = document.getElementById("competences");
-    image.addEventListener("click", grandi)
+    image.addEventListener("click", agrandir);
+}
+
+function agrandir(){
+    let image = document.getElementById("competences");
+    if(image.getAttribute("width") == 567){
+        image.setAttribute("width", 756);
+        image.setAttribute("height", 472);
+    } else {
+        image.setAttribute("width", 567);
+        image.setAttribute("height", 354);
+    }
+    
 }
 
 main();
