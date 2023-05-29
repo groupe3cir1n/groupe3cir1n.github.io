f45d2b70b743afff7b011037bc3d218ae4c0cb5c
Balblaa <vdronne86@orange.fr>
2023-05-29 02:46:30 +0200

    membre ayoub fini

--- script/membres.js
+++ script/membres.js
@@ -1,3 +1,5 @@
+var editeur = 0;
+
 function main(){
 
     // création d'un cercle gris sur le potrait déjà présent
@@ -26,6 +28,46 @@ function get2DContext(id){
 function gratte(e){
     let context = get2DContext("couverture");
     context.clearRect(e.clientX-284, e.pageY-368, 10, 10);
+}
+
+function editor(){  
+
+    if(editeur == 0){
+        let pseudo = prompt("Votre nom de profil :");
+        let pwd = "";
+        if(pseudo == "admin"){
+            pwd = prompt("entrer votre mot de passe :");
+        }
+        if(pwd == "admin_pwd"){
+            editeur = 1
+            mode_editeur();
+        }
+    } else {
+        let sur = confirm("Vous aller quitter le mode edition, êtes vous sûr ?");
+        if(sur){
+            editeur = 0;
+            mode_normal();
+        }
+    }
+}
+
+function mode_editeur(){
+    let button = document.getElementById("b_edit");
+    button.style.backgroundColor = "green";
+    button.style.color = "white";
+    button.innerHTML = "Quitter le mode éditeur";
+    
+    let noms = document.getElementsByClassName("name");
+    for (const nom of noms) {
+        nom.setAttribute("contentEditable", true);
+    }
+}
+
+function mode_normal(){
+    let button = document.getElementById("b_edit");
+    button.style.backgroundColor = "grey";
+    button.style.color = "black";
+    button.innerHTML = "Mode Editeur";
 }
 
 main();
